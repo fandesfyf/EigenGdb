7e56cfb8bbc96ead762d3c9e98fe640fbaa344f9
FanYunFu <2861114322@qq.com>
2024-02-04 11:28:44 +0800

    添加安装脚本

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ~/.gdbinit.d
+cp ./printers.py ~/.gdbinit.d/printers.py
+cp ./__init__.py ~/.gdbinit.d/__init__.py
+echo -en "\
+python \n\
+import sys \n\
+sys.path.insert(0, '/home/${USER}/.gdbinit.d') \n\
+from printers import register_eigen_printers \n\
+register_eigen_printers (None) \n\
+end\n" > ~/.gdbinit
