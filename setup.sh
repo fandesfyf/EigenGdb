4014a9e2415f65aef0f03da382ff606a0338a8bd
FanYunFu <2861114322@qq.com>
2024-04-24 10:05:02 +0800

    add printf

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+echo "Creating GDB init file..."
 mkdir -p ~/.gdbinit.d
 cp ./printers.py ~/.gdbinit.d/printers.py
 cp ./__init__.py ~/.gdbinit.d/__init__.py
@@ -9,3 +11,5 @@ sys.path.insert(0, '/home/${USER}/.gdbinit.d') \n\
 from printers import register_eigen_printers \n\
 register_eigen_printers (None) \n\
 end\n" > ~/.gdbinit
+
+echo "GDB init file created in ~/.gdbinit"
