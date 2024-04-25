6b5313eddcf25d4f7c9d2bdf867a19c0a6f191b9
FanYunFu <2861114322@qq.com>
2024-04-25 22:54:18 +0800

    fix:使用$HOME环境变量代替/home/${USER}

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 echo "Creating GDB init file..."
-mkdir -p ~/.gdbinit.d
-cp ./printers.py ~/.gdbinit.d/printers.py
-cp ./__init__.py ~/.gdbinit.d/__init__.py
+
+mkdir -p "$HOME/.gdbinit.d"
+cp ./printers.py "$HOME/.gdbinit.d/printers.py"
+cp ./__init__.py "$HOME/.gdbinit.d/__init__.py"
 echo -en "\
 python \n\
 import sys \n\
-sys.path.insert(0, '/home/${USER}/.gdbinit.d') \n\
+sys.path.insert(0, '$HOME/.gdbinit.d') \n\
 from printers import register_eigen_printers \n\
 register_eigen_printers (None) \n\
-end\n" > ~/.gdbinit
+end\n" > "$HOME/.gdbinit"
 
-echo "GDB init file created in ~/.gdbinit"
+echo "GDB init file created in $HOME/.gdbinit"
